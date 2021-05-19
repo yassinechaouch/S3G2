5fe4c04980a4c814a575f979b0915ff45259477a
Chadi Hamrouni <chadi.hamrouni@medtech.tn>
2021-05-19 03:46:43 +0100

    updated file structure

---
+++ Documents/Smart_Mirror_App_backup - Copy/src/login.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+private $host = "localhost";
+private $username = "root";
+private $password = "";
+private $dbname = "SMART-MIRROR";
+private $conn;*/
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+// checking creation
+if (mysqli_query($conn, $sql)) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . mysqli_error($conn);
+
+// insertion into the database 
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+}
+
+mysqli_close($conn);
